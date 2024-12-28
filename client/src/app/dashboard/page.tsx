1e14039ed3ca97633c864fa91398ef866e6feff8
zion <atibjawad.zion@optimizely.com>
2024-12-28 22:26:18 +0600

    updated socketio integration

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -4,36 +4,93 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { socket } from "@/websocket";
 import Chart from "@/components/chart";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Dashboard() {
   const router = useRouter();
-  const [stats, setStats] = useState<number[][] | undefined>();
+  const [emailStats, setEmailStats] = useState([{ healthy: true, window: [] }]);
+  const [smsStats, setSmsStats] = useState([{ healthy: true, window: [] }]);
 
-  useEffect(() => {
-    function onStats(stats: string) {
-      try {
-        console.log(JSON.parse(stats));
-        setStats(JSON.parse(stats));
-      } catch (error) {
-        console.log(error);
-      }
+  function onEmailStats(stats: string) {
+    try {
+      setEmailStats(JSON.parse(stats).emailStats);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    socket.on("stats", onStats);
-    return () => {
-      socket.off("stats", onStats);
-    };
-  }, []);
+  function onSmsStats(stats: string) {
+    try {
+      setSmsStats(JSON.parse(stats).smsStats);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
     if (!socket.connected) {
       router.push("/");
+      return;
     }
-  }, [router, socket.connected]);
+
+    socket.on("emailStats", onEmailStats);
+    socket.on("smsStats", onSmsStats);
+    socket.emit("setup");
+
+    return () => {
+      socket.off("emailStats", onEmailStats);
+      socket.off("smsStats", onSmsStats);
+    };
+  }, []);
 
   return (
-    <main>
-      <Chart window={[0, 1, 1]} />
+    <main className="flex flex-col items-center h-full justify-center gap-5">
+      <Card>
+        <CardHeader>
+          <CardTitle>Email Service</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex">
+            {/* queues */}
+            <div className="flex flex-col gap-5">
+              {emailStats.map((provider, index) => (
+                <Chart
+                  key={index}
+                  window={provider.window}
+                  healthy={provider.healthy}
+                />
+              ))}
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>SMS Service</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="flex">
+            {/* queues */}
+            <div className="flex flex-col gap-5">
+              {smsStats.map((provider, index) => (
+                <Chart
+                  key={index}
+                  window={provider.window}
+                  healthy={provider.healthy}
+                />
+              ))}
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </main>
   );
 }
--- client/src/components/chart.tsx
+++ client/src/components/chart.tsx
@@ -1,15 +1,42 @@
-export default function Chart({ window }: { window: number[] }) {
+export default function Chart({
+  window,
+  healthy,
+}: {
+  window: number[];
+  healthy: boolean;
+}) {
   return (
-    <div>
-      {window.map((state, index) => {
-        if (state === 0) {
-          return <div key={index} className="bg-red-500 h-5 w-1" />;
-        } else if (state === 1) {
-          return <div key={index} className="bg-green-500 h-5 w-1" />;
-        } else {
-          return <div key={index} className="bg-gray-500 h-5 w-1" />;
-        }
-      })}
-    </div>
+    <main className="flex flex-col gap-1">
+      <div className="flex gap-[0.1rem]">
+        {window.map((state, index) => {
+          if (state === 0) {
+            return <div key={index} className="bg-red-500 h-10 w-[0.2rem]" />;
+          } else if (state === 1) {
+            return <div key={index} className="bg-green-500 h-10 w-[0.2rem]" />;
+          } else {
+            return <div key={index} className="bg-gray-500 h-10 w-[0.2rem]" />;
+          }
+        })}
+      </div>
+      <div className="flex justify-between w-full text-xs">
+        <span className="flex gap-2 items-center">
+          <div
+            className={`${
+              healthy
+                ? `shadow-[0px_0px_3px_1px_rgba(0,249,0,1)]`
+                : `shadow-[0px_0px_3px_1px_rgba(255,38,0,1)]`
+            }  animate-pulse  rounded-full`}
+          >
+            <div
+              className={`${
+                healthy ? `bg-green-500` : `bg-red-500`
+              } w-1 h-1 rounded-full`}
+            ></div>
+          </div>
+          <p>{healthy ? "Healthy" : "Unhealthy"}</p>
+        </span>
+        <p>Queue size: N</p>
+      </div>
+    </main>
   );
 }
